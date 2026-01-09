82da30c0e2096d6964b10c7ced286ea262859c4e
Mathel-git <mathewajahobasi.com>
2026-01-09 13:17:54 +0100

    Backend ready for deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://land-registry-frontend-ten.vercel.app/", // replace later
+      "https://land-registry-frontend-ten.vercel.app", // replace later
     ],
     credentials: true,
   })
